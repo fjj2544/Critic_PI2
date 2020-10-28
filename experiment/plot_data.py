54a2103622e8671a5562e5393ee7d3f347a1a874
fanjiajun <fanjiajun@bytedance.com>
2020-10-28 18:31:10 +0800

    更改plot

--- experiment/plot_data.py
+++ experiment/plot_data.py
@@ -1,17 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
-ddpg_data = np.array(np.load("./InvertedDoublePendulum/ddpg_data.npy"))
-mpc_data = np.array(np.load("./InvertedDoublePendulum/mpc_data.npy"))
-cpi2_data = np.array(np.load("./InvertedDoublePendulum/reward_data.npy"))
-pi2_data = np.array(np.load("./InvertedDoublePendulum/tra_pi2_data.npy"))
+ENV_NAME = "InvertedDoublePendulum-v1"
+ddpg_data = np.array(np.load(f"/Users/bytedance/Desktop/大四项目/代码/rl/ICRA/Critic_PI2/experiment/data/experiment1/{ENV_NAME}/ddpg_data_smoothed.npy"))
+mpc_data = np.array(np.load(f"/Users/bytedance/Desktop/大四项目/代码/rl/ICRA/Critic_PI2/experiment/data/experiment1/{ENV_NAME}/mpc_data_smoothed.npy"))
+cpi2_data = np.array(np.load(f"/Users/bytedance/Desktop/大四项目/代码/rl/ICRA/Critic_PI2/experiment/data/experiment1/{ENV_NAME}/reward_data_smoothed.npy"))
+pi2_data = np.array(np.load(f"/Users/bytedance/Desktop/大四项目/代码/rl/ICRA/Critic_PI2/experiment/data/experiment1/{ENV_NAME}/tra_pi2_data_smoothed.npy"))
 print(f"ddpg: {ddpg_data.shape}\n mpc: {mpc_data.shape} \n cpi2 {cpi2_data.shape}\n pi2 {pi2_data.shape}")
+
+CONVERGE_VALUE = 900
+data_number = np.inf
 data_list = []
 real_data = []
-data_list.append(cpi2_data[:250])
-data_list.append(mpc_data[:250])
-data_list.append(ddpg_data[:250])
-data_list.append(pi2_data[:250])
-
+data_list.append(cpi2_data)
+data_list.append(mpc_data)
+data_list.append(ddpg_data)
+data_list.append(pi2_data)
+for data in data_list:
+    data_number = min(data_number,len(data))
+data_number = int(data_number)
+for data in data_list:
+    real_data.append(data[:data_number])
+data_list = real_data
 
 label = ["Critic PI2","MPC","DDPG","PI2"]
 color = ["r","g","b","k"]
@@ -21,6 +30,8 @@ linewidth = 1 # 绘图中曲线宽度
 fontsize = 5 # 绘图字体大小
 markersize = 2.5  # 标志中字体大小
 legend_font_size = 5 #图例中字体大小
+labelwidth = 10 # 横纵轴
+
 def mkdir(path):
     import os
     path = path.strip()
@@ -37,21 +48,31 @@ def save_figure(dir,name):
 
 def plot_result(data_list # [algorithm_id,data,*]
                 ,figure_number=3):
+    # plt.figure(figsize=(2.8, 1.7), dpi=300)
     '''绘制alpha曲线'''
-    plt.figure(figsize=(2.8, 1.7), dpi=300)
+    fig, ax = plt.subplots(figsize=(2.8, 1.7), dpi=300)
+    # 取消边框
+    for key, spine in ax.spines.items():
+        # 'left', 'right', 'bottom', 'top'
+        if key == 'right' or key == 'top':
+            spine.set_visible(False)
     plt.xticks(fontproperties='Times New Roman', fontsize=fontsize)
     plt.yticks(fontproperties='Times New Roman', fontsize=fontsize)
-    plt.xlabel("Episodes",fontproperties='Times New Roman',fontsize=fontsize)
-    plt.ylabel("Mean Rewards",fontproperties='Times New Roman',fontsize=fontsize)
+    plt.xlabel("Episodes",fontproperties='Times New Roman',fontsize=labelwidth)
+    plt.ylabel("Mean Rewards",fontproperties='Times New Roman',fontsize=labelwidth)
+    t = np.arange(0, data_number, 1)
     for i in range(figure_number):
-        t = np.arange(0,len(data_list[i])*2,2)
         plt.plot(t,data_list[i], label=label[i],color=color[i],linestyle=line_style[i],linewidth=linewidth)
-    plt.legend(loc='best', prop={'family': 'Times New Roman', 'size': legend_font_size})
-    save_figure("./photo/exp1/", "InvertedDoublePendulum.pdf")
+    converge_line = np.array(np.ones_like(data_list[0]))*CONVERGE_VALUE
+    print(converge_line.shape)
+    plt.plot(t,converge_line,label="converge",color="y",linestyle="--",linewidth=linewidth)
+    # plt.legend(loc='best', prop={'family': 'Times New Roman', 'size': legend_font_size})
+    plt.title(f"{ENV_NAME}",fontproperties='Times New Roman',fontsize=labelwidth)
+    save_figure(f"/Users/bytedance/Desktop/大四项目/代码/rl/ICRA/Critic_PI2/experiment/photo/{ENV_NAME}/", f"{ENV_NAME}.pdf")
     plt.show()
 plot_result(data_list,figure_number=4)
 
-# #--------------------------plot data with tensorboard--------------------------
+#--------------------------plot data with tensorboard--------------------------
 # import tensorflow as tf
 # from datetime import datetime
 # ENV_NAME = "InvertedDoublePendulum-v1" # todo
